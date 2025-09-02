c7b9390284f17a44ea07af3231676d41ce8c81b6
kanzariyabhavy <bhavyakanzariya63@gmail.com>
2025-09-02 09:42:56 +0530

    Add files via upload

---
+++ BIODATA.C
@@ -0,0 +1,18 @@
+#include<stdio.h>  xx
+#include<conio.h>
+void main()
+{
+	clrscr();
+	printf("name: tank madhav");
+	printf("\ndob: 30-12-2007");
+	printf("\nhobby: watching movies");
+	printf("\nstudy: bca");
+	printf("\nlanguage known: gujarati,hindi,english");
+	printf("\naddres: om shiv, gandhinagar,momainagar, street no.3,jamnagar,gujart");
+	printf("\nmobile number: 9428865176");
+	printf("\ninsta id: madhav_30_");
+	printf("\n2insta id: madhav30_pvt");
+	printf("\nemail address: tankmadhav30@gmail.com");
+	printf("\n\n thank you.....");
+	getch();
+}
